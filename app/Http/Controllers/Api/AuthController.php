17a928a5abbfbc06ca726bf724581bf09706214e
castellanolarry7 <castellanolarry7@gmail.com>
2026-08-08 12:31:37 -0400

    Cambios Nulles

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -81,7 +81,7 @@ class AuthController extends Controller
         return response()->json([
             'token'      => $token->plainTextToken,
             'token_type' => 'Bearer',
-            'expires_at' => $token->accessToken->expires_at,
+            'expires_at' => $token->accessToken->expires_at?->toISOString(),
             'user'       => new UserResource($user),
         ], 200);
     }
